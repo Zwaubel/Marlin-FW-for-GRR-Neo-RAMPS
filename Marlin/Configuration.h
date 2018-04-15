f72257a9d574b418715a5de03127f445f98c5f84
Vincent Abt <abt.vincent@gmail.com>
2018-04-15 13:07:45 +0200

    z feedrate adjustment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -581,7 +581,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 30, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 6, 80 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
