969e4ac74cbbdc4e4cdfad74d929cf3fd8d1d3b5
Alejandro Moulier <amoulier@gmail.com>
2021-01-11 06:04:44 -0400

    Date Version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "AM-E3V2-BFix-210110"
+  #define SHORT_BUILD_VERSION "AM-E3V2-BFix-210111"
 #endif
 
 /**
