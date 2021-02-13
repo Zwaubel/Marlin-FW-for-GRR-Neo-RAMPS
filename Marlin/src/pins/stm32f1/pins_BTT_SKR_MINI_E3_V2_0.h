ac6abef999ffe384c09f29a3b9293e0effe859d5
Vincent Abt <abt.vincent@gmail.com>
2021-02-13 23:30:41 +0100

    use FAN1 for the hotend and FAN0 for part cooling

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
@@ -47,10 +47,10 @@
   #define PS_ON_PIN                        PC13  // Power Supply Control
 #endif
 
-#define FAN1_PIN                           PC7
+#define FAN1_PIN                           PC6
 
 #ifndef CONTROLLER_FAN_PIN
-  #define CONTROLLER_FAN_PIN               FAN1_PIN
+  //#define CONTROLLER_FAN_PIN               FAN1_PIN
 #endif
 
 #if HAS_TMC_UART
